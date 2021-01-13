cbc544df82bb8c3e5d261d3e385626688c640ebe
Wade Pate <wadejp8@gmail.com>
2021-01-13 08:07:51 -0700

    Completed the theme switcher .js

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log('Hello World')
+
+let count = '0'
+
+const element = document.getElementById("counter")
+
+const increase = () => {
+  count++
+  element.innerText = count
+}
+
+const decrease = () => {
+  count--
+  element.innerText = count
+}
+
+const reset = () => {
+  count = 0
+  element.innerHTML = "<mark>" + count + "</mark>"
+}
+
+const selectTheme = (theme) => {
+  document.getElementsByTagName("body")[0].className = theme;
+  document.getElementsByTagName("main")[0].className = theme;
+  const buttons = document.getElementsByTagName("button");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].className = theme;
+  }
+}
